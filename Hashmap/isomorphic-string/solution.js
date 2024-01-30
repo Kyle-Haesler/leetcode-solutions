3ff1c64b45dee5f8cc69084f1a17f842d747be46
Kyle Haesler <kyle.haesler@gmail.com>
2024-01-30 13:03:07 -0500

    205 solved

---
+++ Hashmap/isomorphic-string/solution.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const smap = new Map();
+  const tmap = new Map();
+  for (let i = 0; i < s.length; i++) {
+    const schar = s[i];
+    const tchar = t[i];
+    if (smap.has(schar)) {
+      if (smap.get(schar) !== tchar) {
+        return false;
+      }
+    } else {
+      smap.set(schar, tchar);
+    }
+    if (tmap.has(tchar)) {
+      if (tmap.get(tchar) !== schar) {
+        return false;
+      }
+    } else {
+      tmap.set(tchar, schar);
+    }
+  }
+  return true;
+};
