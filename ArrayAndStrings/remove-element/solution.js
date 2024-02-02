ad07e86f061426f359216a99d5c8f3aca8dd0c26
Kyle Haesler <kyle.haesler@gmail.com>
2024-02-02 14:40:34 -0500

    27 solved

--- ArrayAndStrings/remove-element/solution.js
+++ ArrayAndStrings/remove-element/solution.js
@@ -4,16 +4,10 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-  let count = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === val) {
-      nums[i] = "_";
-      count++;
+      nums.splice(i, 1);
+      i--;
     }
   }
-  nums.sort();
-  for (let i = 1; i <= count; i++) {
-    nums.pop();
-  }
-  console.log(nums);
 };
