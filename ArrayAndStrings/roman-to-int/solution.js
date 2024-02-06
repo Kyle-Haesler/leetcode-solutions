247c191b4ba5e8bb69d1a43fa449848d59733d90
Kyle Haesler <kyle.haesler@gmail.com>
2024-02-06 14:35:57 -0500

    13 solved

--- ArrayAndStrings/roman-to-int/solution.js
+++ ArrayAndStrings/roman-to-int/solution.js
@@ -3,22 +3,23 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-  const result = new Map();
-  result.set("I", 1);
-  result.set("V", 5);
-  result.set("X", 10);
-  result.set("L", 50);
-  result.set("C", 100);
-  result.set("D", 500);
-  result.set("M", 1000);
+  const trans = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
   let sum = 0;
   for (let i = 0; i < s.length; i++) {
-    let curr = s[i];
-    let next = s[i + 1];
-    if (result.get(curr) < result.get(next)) {
-      sum -= result.get(curr);
+    const left = s[i];
+    const right = s[i + 1] ? s[i + 1] : null;
+    if (trans[left] < trans[right] && trans[right]) {
+      sum -= trans[left];
     } else {
-      sum += result.get(curr);
+      sum += trans[left];
     }
   }
   return sum;
--- ArrayAndStrings/roman-to-int/solution.py
+++ ArrayAndStrings/roman-to-int/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        result = dict()
-        result["I"] = 1
-        result["V"] = 5
-        result["X"] = 10
-        result["L"] = 50
-        result["C"] = 100
-        result["D"] = 500
-        result["M"] = 1000
+        trans = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
         total = 0
-        for i in range(len(s) - 1):
-            curr = s[i]
-            nex = s[i+1]
-            if result[curr] < result[nex]:
-                total -= result[curr]
+        for i in range(len(s)):
+            left = s[i]
+            right = s[i+1] if i + 1 < len(s) else False
+            if right and trans[left] < trans[right]:
+                total -= trans[left]
             else:
-                total += result[curr]
-        return total + result[s[len(s) - 1]]
-     
+                total += trans[left]
+        return total
