fa4f3b144517db6a63699059337131271a5fb632
Kyle Haesler <kyle.haesler@gmail.com>
2024-02-07 13:40:45 -0500

    58 Solved

--- ArrayAndStrings/length-last-word/solution.py
+++ ArrayAndStrings/length-last-word/solution.py
@@ -1,5 +1,4 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        sList = s.strip().split(" ")
-        print(sList)
-        return len(sList[len(sList) - 1])
+        s = s.strip().split(" ")
+        return len(s[len(s) -1])
