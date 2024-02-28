fa3f6368fd93c83c63ebc491e47d86332974acc2
Kyle Haesler <kyle.haesler@gmail.com>
2024-02-28 16:24:41 -0500

    228 solved

--- Intervals/summary-ranges/solution.js
+++ Intervals/summary-ranges/solution.js
@@ -3,24 +3,24 @@
  * @return {string[]}
  */
 var summaryRanges = function (nums) {
-  let result = [];
+  // alrighty definitely going to use two pointers here to keep track of the start and the end
+  // will push results
+  const res = [];
   let start = 0;
   let end = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i + 1] - nums[i] === 1) {
+    const left = nums[i];
+    const right = i + 1 < nums.length ? nums[i + 1] : null;
+    // check to see if right - left == 1 and right is true meaning we havent gone to the end yet
+    if (right && right - left === 1) {
       end++;
     } else {
-      if (start === end) {
-        result.push(`${nums[start]}`);
-        end++;
-        start++;
-      } else {
-        result.push(`${nums[start]}->${nums[end]}`);
-        start = end + 1;
-        end++;
-      }
+      start === end
+        ? res.push(`${nums[end]}`)
+        : res.push(`${nums[start]}->${nums[end]}`);
+      start = i + 1;
+      end = i + 1;
     }
   }
-
-  return result;
+  return res;
 };
--- Intervals/summary-ranges/solution.py
+++ Intervals/summary-ranges/solution.py
@@ -1,20 +1,19 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
+        # keep track of start and end using pointers
+        # add results as we go
         start = 0
         end = 0
-        result = []
+        res = []
         for i in range(len(nums)):
             left = nums[i]
-            right = nums[i+1] if i + 1 < len(nums) else float("inf")
-            if right - left == 1:
-                end +=1
+            right = nums[i+1] if i +1 < len(nums) else None
+            # check to see if the difference is one
+            if right and right - left == 1:
+                end+=1
             else:
-                if start == end:
-                    result.append(str(left))
-                    start += 1
-                    end += 1
-                else:
-                    result.append("%s->%s" % (str(nums[start]), str(nums[end])))
-                    start = end + 1
-                    end +=1
-        return result
+                res.append("{}".format(nums[end]) if start == end else "{}->{}".format(nums[start],nums[end]))
+                start = i +1
+                end = i +1
+        return res
+            
