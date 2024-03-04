50b6ac21d59337c43cda409cfe16e50a634c9fd7
Kyle Haesler <kyle.haesler@gmail.com>
2024-03-04 15:55:18 -0500

    71 solved

--- stack/simplify-path/solution.py
+++ stack/simplify-path/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         path = path.split("/")
-        
         stack = []
         for char in path:
             if char == "" or char == ".":
                 continue
             elif char == "..":
-                if len(stack) >=1:
+                if len(stack):
                     stack.pop()
                 else:
                     continue
             else:
                 stack.append(char)
-        
         return "/" + "/".join(stack)
